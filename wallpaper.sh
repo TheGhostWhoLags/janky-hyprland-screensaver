005f169399fb07a82a5e861d699025ec6b6a0132
TheGhostWhoLags <118644556+TheGhostWhoLags@users.noreply.github.com>
2026-03-03 21:06:37 +1300

    Update wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 WALLPAPER_DIRECTORY="/home/steam/.config/wallpaper"
+MONITOR="HDMI-A-1"
+
 while true
 do
     WALLPAPER=$(find "$WALLPAPER_DIRECTORY" -type f | shuf -n 1)
-    hyprctl hyprpaper wallpaper "HDMI-A-1, $WALLPAPER, cover"
+    hyprctl hyprpaper wallpaper "$MONITOR, $WALLPAPER, cover"
     sleep 300
 done
-exit
+exit 1
