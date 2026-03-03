4934fceb7f34f21778dd945b144b9c306bb8c906
TheGhostWhoLags <118644556+TheGhostWhoLags@users.noreply.github.com>
2026-03-03 19:29:28 +1300

    Create workspace.sh

---
+++ workspace.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -f /tmp/inputdetect ]; then
+  exit 1
+
+touch /tmp/inputdetect
+
+hyprctl dispatch workspace 9
+trap 'pkill --signal SIGTERM --parent $$' EXIT
+libinput debug-events | grep -m1 "KEY\|MOTION" > /tmp/inputdetect &
+jstest --event /dev/input/js0 | grep -m1 "type 1,\|type 2," > /tmp/inputdetect &
+dbus-monitor | grep -m1 "interface=org.freedesktop.ScreenSaver; member=Inhibit" > /tmp/inputdetect &
+
+systemd-inhibit --what=idle inotifywait -e modify /tmp/inputdetect
+rm /tmp/inputdetect
+hyprctl dispatch workspace 1
+exit
