d5f5dd357e56a91d05234a289eb094915236453d
TheGhostWhoLags <118644556+TheGhostWhoLags@users.noreply.github.com>
2026-03-03 21:02:11 +1300

    Update workspace.sh

--- workspace.sh
+++ workspace.sh
@@ -10,6 +10,7 @@ trap 'pkill --signal SIGTERM --parent $$' EXIT
 libinput debug-events | grep -m1 "KEY\|MOTION" > /tmp/inputdetect &
 jstest --event /dev/input/js0 | grep -m1 "type 1,\|type 2," > /tmp/inputdetect &
 dbus-monitor | grep -m1 "interface=org.freedesktop.ScreenSaver; member=Inhibit" > /tmp/inputdetect &
+./wallpaper.sh &
 
 systemd-inhibit --what=idle inotifywait -e modify /tmp/inputdetect
 rm /tmp/inputdetect
